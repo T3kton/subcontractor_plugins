df06e9ec1cde79880584774ee80347d3323f0fce
Peter Howe <pnhowe@gmail.com>
2017-02-25 21:06:03 -0700

    stubed in virtualbox and iputils

---
+++ virtualbox/__init__.py
@@ -0,0 +1,8 @@
+from subcontractor_plugins.virtualbox.lib import create, destroy
+
+MODULE_NAME = 'virtualbox'
+
+MODULE_FUNCTIONS = {
+                     'create': create,
+                     'destroy': destroy
+                   }
---
+++ virtualbox/lib.py
@@ -0,0 +1,5 @@
+def create( name, disk_list, interface_list ):
+  return True
+
+def destroy( name ):
+  return True
