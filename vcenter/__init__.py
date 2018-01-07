68f9a8ee79bd65c46febe7dca791559b9941acf0
Peter Howe <pnhowe@gmail.com>
2018-01-06 19:48:04 -0700

    removed non functional vcenter

--- vcenter/__init__.py
+++
@@ -1,9 +0,0 @@
-from subcontractor_plugins.vcenter.lib import create, create_rollback, destroy, set_power, power_state
-
-MODULE_FUNCTIONS = {
-                     'create': create,
-                     'create_rollback': create_rollback,
-                     'destroy': destroy,
-                     'set_power': set_power,
-                     'power_state': power_state
-                   }
